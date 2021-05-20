fbbcd3d585226dbcb6652fac1e4d857b093f9a85
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-05-20 18:25:08 -0300

    register form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  return <div className="App">cadastro</div>;
+  const [user, setUser] = useState({ name: "", email: "", birthdate: "" });
+
+  return (
+    <div className="App">
+      <Form user={user} setUser={setUser}/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Form({ user, setUser }) {
+  const { name, email, birthdate } = user;
+  return (
+    <form>
+      <div className="input-box">
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+      </div>
+      <div className="input-box">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+      </div>
+      <div className="input-box">
+        <label htmlFor="birth">Aniversário</label>
+        <input
+          type="date"
+          value={birthdate}
+          onChange={(e) => setUser({ ...user, birthdate: e.target.value })}
+        />
+      </div>
+      <button>Cadastrar</button>
+    </form>
+  );
+}
